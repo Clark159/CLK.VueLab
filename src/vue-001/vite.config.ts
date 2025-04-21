014d68d047b16f60bf1b50ec197826ff8712272e
Clark <clark159@hotmail.com>
2025-04-22 06:47:32 +0800

    x

--- src/vue-001/vite.config.ts
+++ src/vue-001/vite.config.ts
@@ -1,11 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import vueRouter from 'unplugin-vue-router/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vueRouter({ 
