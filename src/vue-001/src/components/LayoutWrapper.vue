561c2998300800cede2b1745219763f8b65c1197
Clark <clark159@hotmail.com>
2025-04-26 12:19:53 +0800

    x

--- src/vue-001/src/components/LayoutWrapper.vue
+++ src/vue-001/src/components/LayoutWrapper.vue
@@ -16,7 +16,7 @@ watchEffect(() => {
 
   // layoutComponent
   layoutComponent.value = defineAsyncComponent(() =>
-    import(`../components/layouts/${layoutName}.vue`)
+    import(`../components/layout/${layoutName}.vue`)
   )
 })
 </script>
