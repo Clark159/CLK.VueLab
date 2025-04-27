73d3e3119e8271141c15d21a09188da8e7fc82cb
Clark <clark159@hotmail.com>
2025-04-28 06:54:53 +0800

    x

--- src/vue-001/src/modules/fbl/ovp-network-http/index.ts
+++ src/vue-001/src/modules/fbl/ovp-network-http/index.ts
@@ -1,19 +1,58 @@
 // import
 import configuration from '@/modules/fbl/ovp-configuration'
-import axios from 'axios'
-import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse } from 'axios'
 
-// class
-class HttpClient {
-  
-}
-
+// Class
 interface HttpClientOptions {
     baseUrl: string
+    timeout?: number
+    headers?: Record<string, string>
 }
 
-// singleton
+class HttpClient {
+
+    // fields
+    private client!: AxiosInstance
+
+    private options!: HttpClientOptions
+
+
+    // constructors
+    constructor() {
+
+        // options
+        this.options = configuration.bind<HttpClientOptions>('fbl.network.http.httpClient')
+        if (!this.options) throw new Error('options is null')
+
+        // client
+        this.client = axios.create({
+            baseURL: this.options.baseUrl,
+            timeout: this.options.timeout ?? 10000,
+            headers: this.options.headers ?? {}
+        })
+    }
+
+
+    // methods
+    async get<T = any>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+        return this.client.get<T>(url, config)
+    }
+
+    async post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+        return this.client.post<T>(url, data, config)
+    }
+
+    async put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+        return this.client.put<T>(url, data, config)
+    }
+
+    async delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+        return this.client.delete<T>(url, config)
+    }
+}
+
+// Singleton
 const httpClient = new HttpClient()
 
-// export
+// Export
 export default httpClient
