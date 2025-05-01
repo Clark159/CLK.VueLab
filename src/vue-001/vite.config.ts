dedf364a7c4318886968c62097461617fae565ce
Clark <clark159@hotmail.com>
2025-05-01 10:13:31 +0800

    x

--- src/vue-001/vite.config.ts
+++ src/vue-001/vite.config.ts
@@ -1,9 +1,9 @@
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import ovpApplication from './src/modules/fbl/ovp-application/vite'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import ovpApplication from './src/modules/fbl/ovp-application/vite'
 
 export default defineConfig({
   plugins: [
